5874ffd6da2f0d02230c1fc0db56dd1b4f81656b
donlHenard <pastor.git101@gmail.com>
2024-02-06 13:01:06 +0800

    Environment dev & prod set

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Todo-list-TOP
 A Project from The Odin Project - Javascript Course - Organizing Your Javscript Code Introduction - OOP Principles
-test
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+
+
+console.log("halo");
+ let example = "I'm a string";
+
+const bodyDom = document.querySelector("body");
+
+bodyDom.textContent = example;
