0927b2faf826460ae5e8d63002c64c4fde67cbf3
donlHenard <pastor.git101@gmail.com>
2024-02-07 19:55:03 +0800

    Add project object creator

---
+++ src/project.js
@@ -0,0 +1,19 @@
+import { createTODO } from "./todo";
+
+const createProject = (title) => {
+    
+    let _title = title;
+    let _allTodos = [];
+
+    const project = {
+        getTitle: () => _title,
+        getTodos: () => _allTodos,
+        addTodo: (todo) => _allTodos.push(todo),
+        removeTodo: (todoToBeRemove) => _allTodos =_allTodos.filter(todo => todo !== todoToBeRemove),
+
+    }
+
+    return project;
+}
+
+export {createProject};
