45a13dff9d26186e7b0607048b4086b3b534f795
donlHenard <pastor.git101@gmail.com>
2024-02-07 12:00:34 +0800

    Add createTODO

---
+++ src/todo.js
@@ -0,0 +1,25 @@
+const createTODO = (title, description, dueDate, priority) => {
+    
+    let _title = title;
+    let _description = description;
+    let _dueDate = dueDate;
+    let _priority = priority;
+    let _project = project;
+
+    const todo = {
+        getTitle: () => _title,
+        setTitle: (newTitle) => { _title = newTitle; },
+        getDescription: () => _description,
+        setDescription: (newDescription) => { _description = newDescription; },
+        getDueDate: () => _dueDate,
+        setDueDate: (newDueDate) => { _dueDate = newDueDate; },
+        getPriority: () => _priority,
+        setPriority: (newPriority) => { _priority = newPriority; },
+        getProject: () => _project,
+        setProject: (newProject) => { _project = newProject}
+    };
+
+    return todo;
+} 
+
+export {createTODO};
